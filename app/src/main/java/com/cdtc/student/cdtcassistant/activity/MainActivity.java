b572365fca7b4ddb6622629f381cac5f1d0791b9
彭陈 <1176262251@qq.com>
2018-04-18 23:50:33 +0800

    1.修改MainActivity界面 2.完成几个常用工具类编写 3.引入常用jar包

--- app/src/main/java/com/cdtc/student/cdtcassistant/activity/MainActivity.java
+++ app/src/main/java/com/cdtc/student/cdtcassistant/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cdtc.student.cdtcassistant.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,23 +11,63 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.cdtc.student.cdtcassistant.R;
+import com.cdtc.student.cdtcassistant.util.T;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    /**
+     * 左侧的头像和文字
+     */
+    private NavigationView navigationView;
+
+    /**
+     * floatingBtn
+     */
+    FloatingActionButton fab;
+    /**
+     * navigation 中的图片
+     */
+    private ImageView headImage;
+
+
+    private Activity activity;
+
+    /**
+     * 按下返回键时间
+     */
+    private long pressBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        initVariable();
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+
+        navigationView.setNavigationItemSelectedListener(this);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,51 +75,64 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+    }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+    private void initVariable() {
+        activity = this;
+    }
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    private void initView() {
+        navigationView  = findViewById(R.id.nav_view);
 
+        fab =  findViewById(R.id.fab);
 
+        //设置Navigation里面的组件要这样获取
+        View headerView= navigationView.getHeaderView(0);
+        headImage = headerView.findViewById(R.id.head_image);
 
+        //测试
+        Glide.with(activity)
+                .load("https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2728030217,1983836315&fm=27&gp=0.jpg")
+                .into(headImage);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            //防止无误触
+            if ((System.currentTimeMillis() - pressBack) > 2000) {
+                T.showShort(activity,"再按一次退出程序");
+                pressBack = System.currentTimeMillis();
+            } else {
+               finish();
+            }
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -86,21 +140,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+//        } else
+        if (id == R.id.nav_tools) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
---
+++ app/src/main/java/com/cdtc/student/cdtcassistant/util/T.java
@@ -0,0 +1,66 @@
+package com.cdtc.student.cdtcassistant.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ *
+ * Toast 工具类
+ * Created by pcc on 2018/4/18.
+ *
+ * @author pcc
+ */
+public class T {
+
+    private static Toast toast;
+
+    /**
+     * 显示一个较短时间的提示
+     * @param context 上下文
+     * @param message 显示的文字
+     */
+    public static void showShort(Context context,String message) {
+        if (toast == null) {
+            toast = Toast.makeText(context,message,Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(message);
+        }
+        toast.show();
+    }
+
+    /**
+     * 显示一个较长时间的提示
+     * @param context 上下文
+     * @param message 显示的文本
+     */
+    public static void showLong(Context context, String message) {
+        if (toast == null) {
+            toast = Toast.makeText(context,message,Toast.LENGTH_LONG);
+        } else {
+            toast.setText(message);
+        }
+        toast.show();
+    }
+
+    /**
+     * 网络错误时展示的提示
+     * @param context
+     */
+    public static void showError(Context context) {
+        if (toast == null) {
+            toast = Toast.makeText(context,"网络错误，请稍后再试",Toast.LENGTH_SHORT);
+        } else {
+            toast.setText("网络错误，请稍后再试");
+        }
+        toast.show();
+    }
+
+    /**
+     * 取消显示
+     */
+    public static void cancel() {
+        if (toast != null) {
+            toast.cancel();
+        }
+    }
+}
