aae5d9b75f3cede01f37339477968c624eb97fff
彭陈 <1176262251@qq.com>
2018-04-22 15:30:36 +0800

    添加  关于我们

---
+++ app/src/main/java/com/cdtc/student/cdtcassistant/activity/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.cdtc.student.cdtcassistant.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cdtc.student.cdtcassistant.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
