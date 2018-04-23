e44ba4018cfdac433e8adf067471b6ef700a4264
彭陈 <1176262251@qq.com>
2018-04-23 23:05:07 +0800

    实现跳蚤商品详细页面

--- app/src/main/java/com/cdtc/student/cdtcassistant/activity/BuyDetailActivity.java
+++ app/src/main/java/com/cdtc/student/cdtcassistant/activity/BuyDetailActivity.java
@@ -3,14 +3,16 @@ package com.cdtc.student.cdtcassistant.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.cdtc.student.cdtcassistant.R;
 
 
 /**
  * 跳蚤市场详细展示页
+ *    后期增加展示图片
  *
  * Created by pcc on 2018/4/22.
  *
@@ -18,7 +20,62 @@ import com.cdtc.student.cdtcassistant.R;
  */
 public class BuyDetailActivity extends BaseTopActivity {
 
+    /**
+     * 标题
+     */
     private String title;
+
+    /**
+     * 商品名称
+     */
+    private TextView name;
+
+    /**
+     * 价格
+     */
+    private TextView price;
+
+    /**
+     * 联系人
+     */
+    private TextView owner;
+
+    /**
+     * 微信
+     */
+    private TextView weChat;
+
+    /**
+     * qq
+     */
+    private TextView qq;
+
+    /**
+     * 电话
+     */
+    private TextView tel;
+
+    /**
+     * 详细描述
+     */
+    private TextView description;
+
+    /**
+     * 微信
+     *  用于控制隐藏
+     */
+    private LinearLayout weChatLayout;
+
+    /**
+     * qq
+     */
+    private LinearLayout qqLayout;
+
+    /**
+     * tel
+     */
+    private LinearLayout telLayout;
+
 
     private Activity activity;
 
@@ -46,6 +103,17 @@ public class BuyDetailActivity extends BaseTopActivity {
 
     private void initView() {
         initTopBar(title);
+
+        qq = getView(R.id.buy_detail_contact_qq);
+        tel = getView(R.id.buy_detail_contact_tel);
+        name = getView(R.id.buy_detail_goods_name);
+        price = getView(R.id.buy_detail_goods_price);
+        owner = getView(R.id.buy_detail_goods_owner);
+        weChat = getView(R.id.buy_detail_contact_wx);
+        qqLayout = getView(R.id.buy_detail_layout_qq);
+        telLayout = getView(R.id.buy_detail_layout_tel);
+        weChatLayout = getView(R.id.buy_detail_layout_wx);
+        description = getView(R.id.buy_detail_goods_description);
     }
 
     public static void startAction(Context context, String title) {
